274f6b73359c445f0a00f4aa4ec4afc049407845
VAegorov <kot2046@gmail.com>
2019-05-07 14:30:44 +0300

    create соединение с БД

--- bd.php
+++ bd.php
@@ -5,3 +5,10 @@
  * Date: 07.05.2019
  * Time: 9:54
  */
+$patch = 'localhost';
+$user = 'root';
+$pass = '';
+$db_name = 'translit';
+
+$link = mysqli_connect($patch, $user, $pass, $db_name);
+mysqli_set_charset($link, 'utf8') or die('Ошибка соединения с БД');
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PC
+ * Date: 07.05.2019
+ * Time: 14:24
+ */
+include_once ('bd.php');
+if ($link) echo "Успех с бд";
