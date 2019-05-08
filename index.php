76df3d9900a41f6ca6e3d485bc30a95796ef90dd
VAegorov <kot2046@gmail.com>
2019-05-08 12:45:48 +0300

    Запрос к БД и отображение выборки

--- index.php
+++ index.php
@@ -7,3 +7,8 @@
  */
 include_once ('bd.php');
 if ($link) echo "Успех с бд";
+$query = "SELECT * FROM slovo";
+$result = mysqli_query($link, $query);
+for ($data = []; $data[] = mysqli_fetch_assoc($result););
+
+include_once ("index.html");
