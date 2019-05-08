d915e04b71c20d173347d30783695a3d5d3cc7a9
VAegorov <kot2046@gmail.com>
2019-05-08 19:38:50 +0300

    Реализован вход и выход admina

--- views/admin.php
+++ views/admin.php
@@ -1,4 +1,4 @@
-<p><a href="../index.php">Вернуться на главную страницу</a></p>
+
 <?php
 /**
  * Created by PhpStorm.
@@ -9,11 +9,20 @@
 
 $hid = "";
 if (isset($_POST["pass"]) && !empty($_POST["pass"]) && $_POST["pass"] === '555') {
-    echo session_start();
+    session_start();
     $_SESSION['auth'] = true;
     $_POST["pass"] = "";
     $hid = "hidden";
     echo "<p><a href='logout.php'>Выход</a></p>";
+    include_once ('../bd.php');
+    $query = "SELECT * FROM slovo";
+    $result = mysqli_query($link, $query);
+    for ($data = []; $data[] = mysqli_fetch_assoc($result););
+    ?>
+    <p><a href="../index.php" <?=$hid; ?>>Вернуться на главную страницу</a></p>
+    <?php
+    include_once ("../index_admin.html");
+
     }
 ?>
 
