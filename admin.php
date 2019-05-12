009cf85399cd088a28391affcdfb8d48cfb2ba7a
VAegorov <kot2046@gmail.com>
2019-05-12 21:47:53 +0300

    Сделана форма редактирования

--- admin.php
+++ admin.php
@@ -6,23 +6,48 @@
  * Date: 08.05.2019
  * Time: 13:31
  */
-
+session_start();
 $hid = "";
-if (isset($_POST["pass"]) && !empty($_POST["pass"]) && $_POST["pass"] === '555') {
-    session_start();
-    $_SESSION['auth'] = true;
-    $_POST["pass"] = "";
+if (isset($_SESSION['auth']) && $_SESSION['auth'] == true) {
     $hid = "hidden";
     echo "<p><a href='logout.php'>Выход</a></p>";
     include_once('bd.php');
+    if (isset($_POST['id']) && !empty($_POST['eng']) && !empty($_POST['eng'])) {
+        $id = (int) $_POST['id'];
+        $eng = mysqli_real_escape_string($link, trim($_POST['eng']));
+        $rus = mysqli_real_escape_string($link, trim($_POST['rus']));
+        $trans = mysqli_real_escape_string($link, trim($_POST['trans']));
+        $t = "UPDATE slovo SET eng='%s', rus='%s', trans='%s' WHERE id=%d";
+        $query = sprintf($t, $eng, $rus, $trans, $id);
+        $result = mysqli_query($link, $query) or die ("Ошибка базы данных");
+        if (mysqli_affected_rows($link) == 1) {
+            echo "Слово обновлено";
+        }
+    }
     $query = "SELECT * FROM slovo";
-    $result = mysqli_query($link, $query);
+    $result = mysqli_query($link, $query) or die ("Ошибка базы данных");
     for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
     ?>
     <p><a href="index.php" <?=$hid; ?>>Вернуться на главную страницу</a></p>
     <?php
     include_once("views/index_admin.html");
+}
 
+if (isset($_POST["pass"]) && !empty($_POST["pass"]) && $_POST["pass"] === '555') {
+    //session_start();
+    $_SESSION['auth'] = true;
+    $_POST["pass"] = "";
+
+    $hid = "hidden";
+    echo "<p><a href='logout.php'>Выход</a></p>";
+    include_once('bd.php');
+    $query = "SELECT * FROM slovo";
+    $result = mysqli_query($link, $query) or die ("Ошибка базы данных");
+    for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
+    ?>
+    <p><a href="index.php" <?=$hid; ?>>Вернуться на главную страницу</a></p>
+    <?php
+    include_once("views/index_admin.html");
     }
 ?>
 
