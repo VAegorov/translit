b2560f752d72cca246412278e3b7e54d473fd74e
VAegorov <kot2046@gmail.com>
2019-05-12 19:22:04 +0300

    Начало формы редактирования

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ include_once ('bd.php');
 //if ($link) echo "Успех с бд";
 
 $query = "SELECT * FROM slovo";
-$result = mysqli_query($link, $query);
+$result = mysqli_query($link, $query) or die("Ошибка базы данных");
 for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
 
 include_once("views/index.html");
