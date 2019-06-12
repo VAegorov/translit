50c801b4ea6d5344911ddc04154da888f5562432
VAegorov <kot2046@gmail.com>
2019-06-12 15:46:37 +0300

    Проверка добавляемого слова на уникальность

--- admin.php
+++ admin.php
@@ -7,14 +7,20 @@
  * Time: 13:31
  */
 session_start();
-if (isset($_SESSION['a']) && $_SESSION['a'] == true) {
-    echo "Слово добавлено";
-    unset($_SESSION['a']);
-    }
+
 
 $hid = "";
 if (isset($_SESSION['auth']) && $_SESSION['auth'] == true) {
     $hid = "hidden";
+    if (isset($_SESSION['a']) && $_SESSION['a'] == true) {
+        echo "Слово добавлено";
+        unset($_SESSION['a']);
+    }
+if (isset($_SESSION['b']) && $_SESSION['b'] == true) {
+        echo "Слово не добавлено. Такое слово уже есть в словаре.";
+        unset($_SESSION['b']);
+    }
+
     echo "<p><a href='logout.php'>Выход</a></p>";
     include_once('bd.php');
 
@@ -40,22 +46,39 @@ if (isset($_SESSION['auth']) && $_SESSION['auth'] == true) {
             echo "Слово удалено";
         }
     }
-
+//adding word
     if (isset($_POST['add']) && (!empty($_POST['eng']) || !empty($_POST['rus']))) {
-        $eng = mysqli_real_escape_string($link, $_POST['eng']);
-        $rus = mysqli_real_escape_string($link, $_POST['rus']);
-        $trans = mysqli_real_escape_string($link, $_POST['trans']);
-        $query =sprintf("INSERT INTO slovo (eng, rus, trans) VALUES ('%s','%s','%s')", $eng, $rus, $trans);
-        mysqli_query($link, $query) or die ("Ошибка базы данных");
-        if (mysqli_affected_rows($link) == 1) {
-            $_SESSION['a'] = true;
+        $eng = mysqli_real_escape_string($link, trim($_POST['eng']));
+        $rus = mysqli_real_escape_string($link, trim($_POST['rus']));
+        $trans = mysqli_real_escape_string($link, trim($_POST['trans']));
+        //проверка наличия добавляемого слова в словаре
+        $query = sprintf("SELECT * FROM slovo WHERE eng='%s'", $eng);
+        $result = mysqli_query($link, $query) or die ("Ошибка базы данных");
+        for ($data = []; $temp = mysqli_fetch_assoc($result); $data[] = $temp);
+
+        if (count($data) > 0) {
+            $_SESSION['b'] = true;
             $host  = $_SERVER['HTTP_HOST'];
             $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
             $extra = 'admin.php';
             header("Location: http://$host$uri/$extra");
+            $_POST = "";
             exit;
+        } else {
+            $query = sprintf("INSERT INTO slovo (eng, rus, trans) VALUES ('%s','%s','%s')", $eng, $rus, $trans);
+            mysqli_query($link, $query) or die ("Ошибка базы данных");
+            if (mysqli_affected_rows($link) == 1) {
+                $_SESSION['a'] = true;
+                $host  = $_SERVER['HTTP_HOST'];
+                $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+                $extra = 'admin.php';
+                header("Location: http://$host$uri/$extra");
+                $_POST = "";
+                exit;
+            }
         }
-        $_POST = "";
+
+
     }
 
     $query = "SELECT * FROM slovo";
@@ -67,24 +90,23 @@ if (isset($_SESSION['auth']) && $_SESSION['auth'] == true) {
     include_once("views/index_admin.html");
 }
 
-if (isset($_POST["pass"]) && !empty($_POST["pass"]) && $_POST["pass"] === '555') {
+if (isset($_POST["pass"]) && !empty($_POST["pass"])) {
     //session_start();
-    $_SESSION['auth'] = true;
-    $_POST["pass"] = "";
+    if ($_POST["pass"] == 555) {
+        $_SESSION['auth'] = true;
+        $_POST["pass"] = "";
+        $host  = $_SERVER['HTTP_HOST'];
+        $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+        $extra = 'admin.php';
+        header("Location: http://$host$uri/$extra");
+        exit;
 
-    $hid = "hidden";
-    echo "<p><a href='logout.php'>Выход</a></p>";
-    include_once('bd.php');
-    $query = "SELECT * FROM slovo";
-    $result = mysqli_query($link, $query) or die ("Ошибка базы данных");
-    for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
-    ?>
-
-
-    <?php
-    include_once("views/index_admin.html");
+    } else {
+        echo "Неправильный пароль! Попробуйте снова.";
     }
-?>
+
+    }
+    ?>
 
 
 <form action="admin.php" method="POST" <?=$hid; ?>>
